33e429a6f17d1b3fc07b9ee35ded4df8eb7fbe7f
ferdiLeCodeur <Ferdi_Codeur@protonmail.com>
2021-10-06 09:42:57 +0200

    plusieurs gagnants

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         $arrayAllPlayer = array();
         $arrayAllPlayer[0] = new Joueur('croumchouszz');
         $arrayAllPlayer[1] = new Joueur('lopette');
-        var_dump($arrayAllPlayer[1]);
+        //var_dump($arrayAllPlayer[1]);
 
         $arraySetAllWin = array();
 
@@ -31,7 +31,7 @@
 
             $mySet = new Set(0,0);
         
-            for ($i=0; $i < 200; $i++) { 
+            for ($i=0; $i < 100; $i++) { 
 
                 if(rand(0,1)) {
                     $mySet->addSetScore(1);
@@ -39,28 +39,32 @@
                     $mySet->addSetScore(2);
                 }
 
-                echo $arrayAllPlayer[0]->afficheNom()." : ". $mySet->getSetScore(1) ." <br />\n";
-                echo $arrayAllPlayer[1]->afficheNom()." : ". $mySet->getSetScore(2) ." <br />\n";
+                // echo $arrayAllPlayer[0]->afficheNom()." : ". $mySet->getSetScore(1) ." <br />\n";
+                // echo $arrayAllPlayer[1]->afficheNom()." : ". $mySet->getSetScore(2) ." <br />\n";
 
                 $gagnant = $mySet->getWinPlayer();
 
                 if($gagnant!= null){
-                    echo " Fin de set! <br />\n";
+                    // echo " Fin de set! <br />\n";
                     
                     if($gagnant == 1){
                         echo " est gagnant du set  ". $arrayAllPlayer[0]->afficheNom() ."<br />\n";
                         array_push($arraySetAllWin, 1);
+                        $mySet = new Set(0,0);
                         
                     }else{
                         echo " est gagnant du set  ". $arrayAllPlayer[1]->afficheNom() ."<br />\n";
                         array_push($arraySetAllWin, 2);
-                        
+                        $mySet = new Set(0,0);
                     }
                 }
-                var_dump($arraySetAllWin);
-                
+
+    
             }
-        }       
+        }
+        // Si dans le tableau le 1 ou le 2 sorte 3 fois 
+        // on trouve un gagnant
+
     ?>
 </body>
 
